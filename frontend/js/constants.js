97a502da4cba8cbf55386529fecbd5323975cb84
cisxy <botyachtclub@gmail.com>
2022-05-06 23:22:39 -0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x3495a633eDD28C116beC280425349C9C04d1e799"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
