b2e64b28c064cfc649c46b450a338330e1b1c51d
cisxy <botyachtclub@gmail.com>
2022-05-09 00:33:37 -0400

    updated contract address on config js

--- backend/src/config.js
+++ backend/src/config.js
@@ -69,7 +69,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0x3495a633eDD28C116beC280425349C9C04d1e799"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x00ABab6DAb00D93d22BE35c14975A259023C061f"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
